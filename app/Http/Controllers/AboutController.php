4b15f0657ab470932480b38ee0148086a69fd12b
Wakhid Hasim <wakhidhasim@Wakhids-MacBook-Pro.local>
2023-02-26 01:31:36 +0700

    remove comment from controller

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -2,16 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class AboutController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function __invoke()
     {
         return view('pages.about');
--- app/Http/Controllers/DetailPortfolioController.php
+++ app/Http/Controllers/DetailPortfolioController.php
@@ -2,16 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 
 class DetailPortfolioController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function __invoke()
     {
         return view('pages.portfolio-detail');
